98883b9a14b622c8a7fbd106eb0cb2d1825bf414
Robert Ledezma <robert.anibal.ledezma@gmail.com>
2025-03-18 23:03:51 +0100

    Class component creada

---
+++ src/js/cardInfo.js
@@ -0,0 +1,34 @@
+export class Card{
+    constructor(data){
+        this.name = data.name;
+        this.description = data.description;
+        this.image = data.image;
+        this.link = '/section/'+data.name.toLowerCase();
+    }
+
+    createCard() {
+        const card = document.createElement('div');
+        card.classList.add('card', 'especial-card');
+
+        card.innerHTML = `
+                <img src="${this.image}" alt="Project" class="img-card">
+                <div class="card-content">
+                    <div class="copy">
+                        <p class="body-medium">${this.name}</p>
+                        <p class="headline-regular">${this.description}</p>
+                    </div>
+                    <div class="link">
+                        <a href="${this.link}">Learn more</a>
+                    </div>
+                </div>
+        `
+
+        return card;
+
+    }
+
+    addDomCard(){
+        const cardContainer = document.querySelector('.card-container');
+        cardContainer.appendChild(this.createCard());
+    }
+}
